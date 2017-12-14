c903ec02c3bb3e9f09b0711618e8b4ba53c6742d
Trina <tcjcastillo@gmail.com>
2017-12-13 23:03:42 -0800

    truncate favorites text

--- src/components/review-forms-modal-button.js
+++ src/components/review-forms-modal-button.js
@@ -9,87 +9,85 @@ import {
     ModalCardBody,
     ModalCardFooter,
     ModalCardHeader,
-    ModalCardTitle
+    ModalCardTitle,
 } from 'bloomer'
 import StyledIcon from './styled-icon'
 
 class FormsButton extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { showModal: false }
-    }
+  constructor(props) {
+    super(props)
+    this.state = { showModal: false }
+  }
 
-    handleClick = () => {
-        this.setState(({ showModal }) => ({ showModal: !showModal }))
-    }
+  handleClick = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }))
+  }
 
-    render() {
-        return (
-            <div>
-                <Button
-                    style={{ margin: `1rem 0` }}
-                    isSize="small"
-                    isColor="primary"
-                    isOutlined={true}
-                    onClick={this.handleClick}
-                >
-                    <StyledIcon name="book" />
-                    Review Forms
-                </Button>
+  render() {
+    return (
+      <div>
+        <Button
+          style={{ margin: `1rem 0` }}
+          isSize="small"
+          isColor="primary"
+          isOutlined={true}
+          onClick={this.handleClick}
+        >
+          <StyledIcon name="book" />
+          Review Forms
+        </Button>
 
-                <Modal isActive={this.state.showModal}>
-                    <ModalBackground onClick={this.handleClick} />
-                    <ModalCard>
-                        <ModalCardHeader>
-                            <ModalCardTitle>
-                                <Heading style={{ marginBottom: 0 }}>Documents</Heading>
-                            </ModalCardTitle>
-                        </ModalCardHeader>
-                        <ModalCardBody>
-                            <Content>
-                                <p>
-                                    Single origin, eu coffee robusta dripper milk espresso at
-                                    irish, redeye saucer, brewed cup in sit coffee. Turkish
-                                    barista acerbic black id, café au lait robust grinder organic,
-                                    seasonal beans latte strong filter. Half and half, galão,
-                                    seasonal frappuccino shop cup, trifecta cup iced, shop irish
-                                    single shot est medium kopi-luwak americano barista grounds.
-                                    Bar mazagran milk dripper froth, grinder brewed aromatic
-                                    mazagran in kopi-luwak mug aromatic. Sweet aromatic grinder
-                                    fair trade, dripper bar plunger pot spoon irish in at, strong
-                                    aroma flavour, french press so, coffee, galão et qui ristretto
-                                    affogato lungo. Sit sweet, shop et, cream roast, aromatic est
-                                    single shot whipped, sugar coffee fair trade iced
-                                    decaffeinated carajillo breve. Single origin french press
-                                    americano brewed cortado in caramelization, cappuccino mocha
-                                    macchiato sweet americano froth ristretto id coffee viennese
-                                    brewed single shot. Crema coffee brewed white con panna,
-                                    decaffeinated to go at, steamed java, organic coffee, trifecta
-                                    ut blue mountain breve organic grinder filter robusta coffee.
-                                    Aromatic, coffee, to go dark robust frappuccino espresso,
-                                    java, aftertaste breve redeye, roast variety mug, pumpkin
-                                    spice and mug, white mug, percolator americano fair trade
-                                    siphon kopi-luwak. Barista macchiato seasonal, french press,
-                                    kopi-luwak, con panna body, siphon iced et body viennese
-                                    sweet, carajillo wings aged, carajillo aged coffee fair trade
-                                    macchiato.
-                                </p>
-                            </Content>
-                        </ModalCardBody>
-                        <ModalCardFooter>
-                            <Button isColor="primary" onClick={this.handleClick}>
-                                OK
-                            </Button>
-                        </ModalCardFooter>
-                    </ModalCard>
-                </Modal>
-            </div>
-        )
-    }
+        <Modal isActive={this.state.showModal}>
+          <ModalBackground onClick={this.handleClick} />
+          <ModalCard>
+            <ModalCardHeader>
+              <ModalCardTitle>
+                <Heading style={{ marginBottom: 0 }}>Review Documents</Heading>
+              </ModalCardTitle>
+            </ModalCardHeader>
+            <ModalCardBody>
+              <Content>
+                <p>
+                  Single origin, eu coffee robusta dripper milk espresso at
+                  irish, redeye saucer, brewed cup in sit coffee. Turkish
+                  barista acerbic black id, café au lait robust grinder organic,
+                  seasonal beans latte strong filter. Half and half, galão,
+                  seasonal frappuccino shop cup, trifecta cup iced, shop irish
+                  single shot est medium kopi-luwak americano barista grounds.
+                  Bar mazagran milk dripper froth, grinder brewed aromatic
+                  mazagran in kopi-luwak mug aromatic. Sweet aromatic grinder
+                  fair trade, dripper bar plunger pot spoon irish in at, strong
+                  aroma flavour, french press so, coffee, galão et qui ristretto
+                  affogato lungo. Sit sweet, shop et, cream roast, aromatic est
+                  single shot whipped, sugar coffee fair trade iced
+                  decaffeinated carajillo breve. Single origin french press
+                  americano brewed cortado in caramelization, cappuccino mocha
+                  macchiato sweet americano froth ristretto id coffee viennese
+                  brewed single shot. Crema coffee brewed white con panna,
+                  decaffeinated to go at, steamed java, organic coffee, trifecta
+                  ut blue mountain breve organic grinder filter robusta coffee.
+                  Aromatic, coffee, to go dark robust frappuccino espresso,
+                  java, aftertaste breve redeye, roast variety mug, pumpkin
+                  spice and mug, white mug, percolator americano fair trade
+                  siphon kopi-luwak. Barista macchiato seasonal, french press,
+                  kopi-luwak, con panna body, siphon iced et body viennese
+                  sweet, carajillo wings aged, carajillo aged coffee fair trade
+                  macchiato.
+                </p>
+              </Content>
+            </ModalCardBody>
+            <ModalCardFooter isDisplay="block" hasTextAlign="right">
+              <Button isColor="primary" onClick={this.handleClick}>
+                OKay
+              </Button>
+            </ModalCardFooter>
+          </ModalCard>
+        </Modal>
+      </div>
+    )
+  }
 }
 
-FormsButton.propTypes = {
-}
+FormsButton.propTypes = {}
 
-export default FormsButton;
-
+export default FormsButton
--- src/pages/favorites.js
+++ src/pages/favorites.js
@@ -2,72 +2,69 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 import { Box, Column, Columns, Container, Heading, Section } from 'bloomer'
+import Dotdotdot from 'react-dotdotdot'
 import { serifFont } from '../utils/fonts'
 import { colorBrown, colorBrownDark, colorGreenDark, } from '../utils/theme-variables'
 import PageHeader from '../components/page-header'
 
+const cardWrapperStyles = {
+  position: 'relative',
+  width: '100%',
+  color: colorBrown,
+}
+const cardImageStyles = thumbnail => ({
+  height: '300px',
+  overflowY: 'hidden',
+  background: `url(${thumbnail}) center no-repeat`,
+  backgroundSize: 'cover',
+})
+const cardOverlayStyles = {
+  padding: '1.5em',
+  overflowY: 'hidden',
+  position: 'absolute',
+  top: '70%',
+  height: '30%',
+  left: '0',
+  width: '100%',
+  background: 'rgba(255,255,255,0.9)',
+  hasTextAlign: 'center',
+  transition: 'top 0.3s, height 0.3s',
+  '&:hover': {
+    height: '100%',
+    top: '0',
+  },
+}
+const headingStyles = {
+  ...serifFont,
+  lineHeight: '120%',
+  color: colorGreenDark,
+  marginBottom: 0,
+  fontSize: '1.75rem',
+}
+const titleBorderStyles = {
+  margin: `1rem 0`,
+  borderBottom: `1px solid rgb(79, 109, 26)`,
+  width: `1.75rem`,
+  padding: 0,
+}
+const excerptStyles = {
+  fontSize: `1.2rem`,
+  textTransform: 'none',
+  color: colorBrownDark,
+}
+
 const FavoriteCard = ({ slug, thumbnail, title, excerpt }) => (
   <Link to={slug}>
-    <div
-      css={{
-        position: 'relative',
-        width: '100%',
-        color: colorBrown,
-      }}
-    >
+    <div css={cardWrapperStyles}>
       <Box>
-        <div
-          css={{
-            height: '300px',
-            overflowY: 'hidden',
-            background: `url(${thumbnail}) center no-repeat`,
-            backgroundSize: 'cover',
-          }}
-        />
-        <div
-          css={{
-            opacity: 0,
-            padding: '1.5em',
-            overflowY: 'hidden',
-            position: 'absolute',
-            top: '0',
-            left: '0',
-            width: '100%',
-            height: '100%',
-            background: 'rgba(255,255,255,0.9)',
-            hasTextAlign: 'center',
-            transition: 'opacity 0.3s',
-            '&:hover': {
-              opacity: 1,
-            },
-          }}
-        >
-          <Heading
-            style={{
-              ...serifFont,
-              color: colorGreenDark,
-              marginBottom: 0,
-              fontSize: '1.75rem',
-            }}
-          >
-            {title}
+        <div css={cardImageStyles(thumbnail)} />
+        <div css={cardOverlayStyles}>
+          <Heading style={headingStyles}>
+            <Dotdotdot clamp={4}>{title}</Dotdotdot>
           </Heading>
-          <div
-            css={{
-              margin: `1rem 0`,
-              borderBottom: `1px solid rgb(79, 109, 26)`,
-              width: `1.75rem`,
-              padding: 0,
-            }}
-          />
-          <p
-            css={{
-              fontSize: `1.2rem`,
-              textTransform: 'none',
-              color: colorBrownDark,
-            }}
-          >
-            {excerpt}
+          <div css={titleBorderStyles} />
+          <p css={excerptStyles}>
+            <Dotdotdot clamp={5}>{excerpt}</Dotdotdot>
           </p>
         </div>
       </Box>
@@ -92,7 +89,12 @@ const FavoritesTemplate = ({ data }) => {
         >
           {edges.map(({ node }) => (
             <Column
-              isSize={{ mobile: 1, tablet: '1/3', desktop: '1/4' }}
+              isSize={{
+                mobile: 1,
+                tablet: '1/3',
+                desktop: '1/4',
+                widescreen: 3,
+              }}
               key={node.id}
             >
               <FavoriteCard
@@ -122,7 +124,7 @@ export const pageQuery = graphql`
       edges {
         node {
           id
-          excerpt(pruneLength: 100)
+          excerpt(pruneLength: 70)
           frontmatter {
             title
             link
