6b4fc31f3c9b095815aadbbeab40c5ed96996606
Trina <tcjcastillo@gmail.com>
2017-12-13 23:16:20 -0800

    hyperlink hack

--- src/templates/favorite-item.js
+++ src/templates/favorite-item.js
@@ -1,103 +1,84 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import { Box, Button, Column, Columns, Container, Content, Image, Section, Title, } from 'bloomer'
+import { Box, Button, Container, Content, Image, Section, Title, } from 'bloomer'
 import Link, { withPrefix } from 'gatsby-link'
 import { secondaryFont, serifFont } from '../utils/fonts'
 import { colorGreenDark } from '../utils/theme-variables'
 import StyledIcon from '../components/styled-icon'
 
-const settings = {
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  lazyLoad: false,
-}
 export default function FavoriteItemTemplate({ data }) {
   const { markdownRemark: post } = data
-  const { images } = post.frontmatter
+  const { images, link } = post.frontmatter
+  let hyperlink = link.indexOf('http') >= 0 ? link : `http://${link}`
+
   return (
     <Section className="section">
       <Helmet title={`Favorites | ${post.frontmatter.title}`} />
-      <Container>
-        <Columns>
-          <Column isSize={5} isOffset={1} style={{}}>
-            <Link
-              style={{
-                ...serifFont,
-                fontSize: '1.2rem',
-                color: colorGreenDark,
-              }}
-              to="/favorites/"
+      <Container isClearfix={true}>
+        <div>
+          {images.map((t, index) => (
+            <div
+              style={{ float: 'right', margin: '0 0 1rem 1.5rem' }}
+              key={t.image}
             >
-              <StyledIcon
-                styles={{ marginBottom: `1.5rem` }}
-                name="angle-left"
-              />
-              {` `}
-              Back to Favorites
-            </Link>
-
-            <Title
-              style={{ ...secondaryFont, marginBottom: '0.75rem' }}
-              isSize={2}
-              hasTextColor="dark"
-            >
-              {post.frontmatter.title}
-            </Title>
-            <Content dangerouslySetInnerHTML={{ __html: post.html }} />
-            <Button
-              style={{ marginBottom: '1.5em' }}
-              isColor="primary"
-              href={post.frontmatter.link}
-              target="_blank"
-            >
-              <StyledIcon
-                name="shopping-cart"
-              />Buy Item
-            </Button>
-            <div>
-              <p>
-                <small>Share On:</small>
-              </p>
-              <Link style={{ paddingRight: '0.5em' }} to="">
-                <StyledIcon
-                  name="facebook-official"
-                  style={{ fontSize: '1.25em' }}
+              <Box style={{ maxWidth: '450px', maxHeight: '700px' }}>
+                <Image
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                    border: '1px solid gainsboro',
+                  }}
+                  src={__PATH_PREFIX__ + t.image}
+                  alt={post.frontmatter.title + ' ' + (index + 1)}
                 />
-              </Link>
-              <Link style={{ padding: '0.5em' }} to="">
-                <span
-                  name="pinterest"
-                  style={{ fontSize: '1.25em' }}
-                />
-              </Link>
+              </Box>
             </div>
-          </Column>
-          <Column isSize={5} style={{ padding: '0' }}>
-            {/*<Slider
-              dots={images.length > 1}
-              arrows={images.length > 1}
-              infinite={false}
-              {...settings}
-            >
-            </Slider>*/}
-            {images.map((t, index) => (
-              <div style={{ width: 'auto' }} key={t.image}>
-                <Box style={{ maxWidth: '450px', maxHeight: '700px' }}>
-                  <Image
-                    style={{
-                      width: '100%',
-                      height: '100%',
-                      border: '1px solid gainsboro',
-                    }}
-                    src={__PATH_PREFIX__ + t.image}
-                    alt={post.frontmatter.title + ' ' + (index + 1)}
-                  />
-                </Box>
-              </div>
-            ))}
-          </Column>
-        </Columns>
+          ))}
+
+          <Link
+            style={{
+              ...serifFont,
+              fontSize: '1.2rem',
+              color: colorGreenDark,
+            }}
+            to="/favorites/"
+          >
+            <StyledIcon styles={{ marginBottom: `1.5rem` }} name="angle-left" />
+            {` `}
+            Back to Favorites
+          </Link>
+
+          <Title
+            style={{ ...secondaryFont, marginBottom: '0.75rem' }}
+            isSize={2}
+            hasTextColor="dark"
+          >
+            {post.frontmatter.title}
+          </Title>
+          <Button
+            style={{ margin: '1em 0' }}
+            isColor="primary"
+            href={hyperlink}
+            target="_blank"
+          >
+            <StyledIcon name="shopping-cart" />Buy Item
+          </Button>
+          <Content dangerouslySetInnerHTML={{ __html: post.html }} />
+          <div>
+            <p>
+              <small>Share On:</small>
+            </p>
+            <Link style={{ paddingRight: '0.5em' }} to="">
+              <StyledIcon
+                name="facebook-official"
+                style={{ fontSize: '1.25em' }}
+              />
+            </Link>
+            <Link style={{ padding: '0.5em' }} to="">
+              <span name="pinterest" style={{ fontSize: '1.25em' }} />
+            </Link>
+          </div>
+        </div>
       </Container>
     </Section>
   )
