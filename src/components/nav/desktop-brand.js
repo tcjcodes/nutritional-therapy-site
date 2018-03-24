9d1cd15f0cfb9ba16807a380921fa37438fc87d9
Trina <tcjcastillo@gmail.com>
2018-03-24 15:29:59 -0700

    component-ize Nav

---
+++ src/components/nav/desktop-brand.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import BrandItem from './brand-item';
+
+const DesktopBrand = ({ title }) => (
+  <div
+    className="is-hidden-touch"
+    css={{
+      width: '100%',
+      marginBottom: `0.5rem`,
+      textAlign: 'center',
+      paddingTop: '1.5em',
+    }}
+  >
+    <BrandItem title={title} />
+  </div>
+);
+
+export default DesktopBrand;
---
+++ src/components/nav/tablet-navbar-brand.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavbarBrand, NavbarBurger, NavbarItem } from 'bloomer';
+import BrandItem from './brand-item';
+
+const TabletNavbarBrand = ({ title, isOpen, onMenuClick }) => (
+  <NavbarBrand isHidden="desktop">
+    <NavbarBurger
+      style={{ marginRight: 'auto', marginLeft: 'inherit' }}
+      isActive={isOpen}
+      onClick={onMenuClick}
+    />
+
+    <NavbarItem style={{ marginRight: 'auto' }}>
+      <BrandItem title={title} />
+    </NavbarItem>
+  </NavbarBrand>
+);
+
+TabletNavbarBrand.propTypes = {};
+
+export default TabletNavbarBrand;
