fa0b80bd5b823d724752cb08da5578baa48b0c2e
Trina <tcjcastillo@gmail.com>
2018-03-24 16:01:02 -0700

    styling Product template

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,25 +1,42 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { Box, Button, Container, Content, Image, Section, Title } from 'bloomer'
-import Link, { withPrefix } from 'gatsby-link'
-import { secondaryFont, serifFont } from '../utils/fonts'
-import { colorGreenDark } from '../utils/theme-variables'
-import StyledIcon from '../components/styled-icon'
+import React from 'react';
+import Helmet from 'react-helmet';
+import {
+  Box,
+  Button,
+  Column,
+  Columns,
+  Container,
+  Content,
+  Image,
+  Section,
+  Title,
+} from 'bloomer';
+import Link, { withPrefix } from 'gatsby-link';
+import { secondaryFont, serifFont } from '../utils/fonts';
+import { colorGreenDark } from '../utils/theme-variables';
+import StyledIcon from '../components/styled-icon';
 
 export default function ProductTemplate({ data }) {
-  const siteTitle = data.site.siteMetadata.title
+  const siteTitle = data.site.siteMetadata.title;
 
-  const { markdownRemark: post } = data
-  const { image, link, title } = post.frontmatter
-  let hyperlink = link.indexOf('http') >= 0 ? link : `http://${link}`
+  const { markdownRemark: post } = data;
+  const { image, link, title } = post.frontmatter;
+  let hyperlink = link.indexOf('http') >= 0 ? link : `http://${link}`;
 
   return (
     <Section className="section">
       <Helmet title={`Product | ${siteTitle}`} />
-      <Container isClearfix={true}>
-        <div>
-          <div style={{ float: 'left', margin: '0 1.5rem 0.5rem 0' }}>
-            <Box style={{ maxWidth: '450px', maxHeight: '700px' }}>
+      <Container style={{ maxWidth: 800 }}>
+        <Columns isVCentered isCentered>
+          <Column
+            isSize={{ desktop: 6, mobile: 12 }}
+            style={{ margin: '1em 0' }}
+          >
+            <Box
+              style={{
+                margin: '0 0.5rem 0.5rem 0',
+              }}
+            >
               <Image
                 style={{
                   width: '100%',
@@ -30,52 +47,61 @@ export default function ProductTemplate({ data }) {
                 alt={post.frontmatter.title}
               />
             </Box>
-          </div>
-          <Link
-            style={{
-              ...serifFont,
-              fontSize: '1.2rem',
-              color: colorGreenDark,
-            }}
-            to={`/product-categories/${post.fields.categoryKey}`}
-          >
-            <StyledIcon styles={{ marginBottom: `1.5rem` }} name="angle-left" />
-            {` `}
-            Go Back
-          </Link>
+          </Column>
 
-          <Title
-            style={{ ...secondaryFont, marginBottom: '0.75rem' }}
-            isSize={2}
-            hasTextColor="dark"
-          >
-            {post.frontmatter.title}
-          </Title>
-          <div>
-            <span css={{ marginRight: '1em' }}>Share On:</span>
-            <Link style={{ paddingRight: '0.5em' }} to="">
+          <Column isSize={{ desktop: 6, mobile: 12 }}>
+            <Link
+              style={{
+                ...serifFont,
+                fontSize: '1.2rem',
+                color: colorGreenDark,
+              }}
+              to={`/product-categories/${post.fields.categoryKey}`}
+            >
               <StyledIcon
-                name="facebook-official"
-                style={{ fontSize: '1.25em' }}
+                styles={{ marginBottom: `1.5rem` }}
+                name="angle-left"
               />
+              {` `}
+              Go Back
             </Link>
-            <Link style={{ padding: '0.5em' }} to="">
-              <span name="pinterest" style={{ fontSize: '1.25em' }} />
-            </Link>
-          </div>
-          <Button
-            style={{ margin: '1em 0' }}
-            isColor="primary"
-            href={hyperlink}
-            target="_blank"
-          >
-            <StyledIcon name="shopping-cart" />Buy Item
-          </Button>
+
+            <Title
+              style={{ ...secondaryFont, marginBottom: '0.75rem' }}
+              isSize={2}
+              hasTextColor="dark"
+            >
+              {post.frontmatter.title}
+            </Title>
+            <div>
+              <span css={{ marginRight: '1em' }}>Share On:</span>
+              <Link style={{ paddingRight: '0.5em' }} to="">
+                <StyledIcon
+                  name="facebook-official"
+                  style={{ fontSize: '1.25em' }}
+                />
+              </Link>
+              <Link style={{ padding: '0.5em' }} to="">
+                <span name="pinterest" style={{ fontSize: '1.25em' }} />
+              </Link>
+            </div>
+            <Button
+              style={{ margin: '1em 0' }}
+              isColor="primary"
+              href={hyperlink}
+              target="_blank"
+            >
+              <StyledIcon name="shopping-cart" />Buy Item
+            </Button>
+          </Column>
+        </Columns>
+
+        <div>
           <Content dangerouslySetInnerHTML={{ __html: post.html }} />
         </div>
       </Container>
     </Section>
-  )
+  );
 }
 
 export const pageQuery = graphql`
@@ -98,4 +124,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
