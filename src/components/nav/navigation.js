98fedfc0cd1039f5c435af37753ccaa3c32a16e2
Trina C <tcjcastillo@gmail.com>
2022-01-02 15:07:02 -0800

    change main nav, remove addr from contact

--- src/components/nav/navigation.js
+++ src/components/nav/navigation.js
@@ -1,11 +1,11 @@
-import { Navbar, NavbarMenu } from 'bloomer'
-import React from 'react'
-import { serifFont } from '../../utils/fonts'
-import { colorBrownDark } from '../../utils/theme-variables'
-import DesktopBrand from './desktop-brand'
-import NavbarDropdownItem from './navbar-dropdown-item'
-import StyledNavbarItem from './styled-navbar-item'
-import TabletNavbarBrand from './tablet-navbar-brand'
+import { Navbar, NavbarMenu } from 'bloomer';
+import React from 'react';
+import { serifFont } from '../../utils/fonts';
+import { colorBrownDark } from '../../utils/theme-variables';
+import DesktopBrand from './desktop-brand';
+import NavbarDropdownItem from './navbar-dropdown-item';
+import StyledNavbarItem from './styled-navbar-item';
+import TabletNavbarBrand from './tablet-navbar-brand';
 
 const linkProps = {
   style: {
@@ -15,7 +15,7 @@ const linkProps = {
   activeStyle: {
     color: colorBrownDark,
   },
-}
+};
 
 const clientsNavItems = [
   {
@@ -26,7 +26,7 @@ const clientsNavItems = [
     text: 'Lab Testing',
     to: 'labs',
   },
-]
+];
 
 const Navigation = ({ onMenuClick, isOpen, categoryNodes, title }) => {
   const productNavItems = categoryNodes.map((node) => {
@@ -34,8 +34,8 @@ const Navigation = ({ onMenuClick, isOpen, categoryNodes, title }) => {
       id: node.id,
       text: node.frontmatter.name,
       to: `product-categories/${node.fields.categoryKey}`,
-    }
-  })
+    };
+  });
   return (
     <div>
       <DesktopBrand title={title} />
@@ -53,42 +53,49 @@ const Navigation = ({ onMenuClick, isOpen, categoryNodes, title }) => {
         />
 
         <NavbarMenu
-            data-testid="desktop-navbar"
+          data-testid="desktop-navbar"
           style={{ justifyContent: 'center' }}
           isActive={isOpen}
           onClick={onMenuClick}
         >
           <StyledNavbarItem {...linkProps} text="About" />
           <StyledNavbarItem {...linkProps} text="Services" />
-          <NavbarDropdownItem
-            {...linkProps}
-            text="Clients"
-            items={clientsNavItems}
-          />
+
           <NavbarDropdownItem
             {...linkProps}
             text="Favorites"
             to="products"
             items={productNavItems}
           />
+          <StyledNavbarItem
+            {...linkProps}
+            text="New Clients"
+            to="client-forms"
+          />
+          <StyledNavbarItem {...linkProps} text="Labs" to="labs" />
+          {/*<NavbarDropdownItem*/}
+          {/*  {...linkProps}*/}
+          {/*  text="Clients"*/}
+          {/*  items={clientsNavItems}*/}
+          {/*/>*/}
           <StyledNavbarItem {...linkProps} text="Contact" />
         </NavbarMenu>
       </Navbar>
     </div>
-  )
-}
+  );
+};
 
 class NavigationContainer extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = { isOpen: false }
+    super(props);
+    this.state = { isOpen: false };
   }
 
   onMenuClick = () => {
     this.setState(({ isOpen: prevIsOpen }) => ({
       isOpen: !prevIsOpen,
-    }))
-  }
+    }));
+  };
 
   render() {
     return (
@@ -97,8 +104,8 @@ class NavigationContainer extends React.Component {
         onMenuClick={this.onMenuClick}
         {...this.props}
       />
-    )
+    );
   }
 }
 
-export default NavigationContainer
+export default NavigationContainer;
