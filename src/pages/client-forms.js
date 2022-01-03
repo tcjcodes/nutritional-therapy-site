dbc66a594de4071091362c4915eec0153d105c81
Trina C <tcjcastillo@gmail.com>
2022-01-02 16:14:24 -0800

    feat(new clients): fix contact link

--- src/pages/client-forms.js
+++ src/pages/client-forms.js
@@ -1,18 +1,20 @@
-import { Column, Columns, Container, Section, Content, Box } from 'bloomer'
-import React from 'react'
-import Helmet from 'react-helmet'
-import Img from 'gatsby-image'
-import { graphql } from 'gatsby'
-import Layout from '../components/layout'
-import PageHeader from '../components/page-header'
+import { Column, Columns, Container, Section, Content, Box } from 'bloomer';
+import Link from 'gatsby-link';
+import React from 'react';
+import Helmet from 'react-helmet';
+import Img from 'gatsby-image';
+import { graphql } from 'gatsby';
+import Layout from '../components/layout';
+import PageHeader from '../components/page-header';
 
+const pageTitle = 'New Clients';
 const ClientFormsPage = ({ data }) => {
-  const siteTitle = data.site.siteMetadata.title
-  const imgSizes = data.file.childImageSharp.fluid
+  const siteTitle = data.site.siteMetadata.title;
+  const imgSizes = data.file.childImageSharp.fluid;
   return (
     <Layout>
       <Section>
-        <Helmet title={`Client Forms | ${siteTitle}`} />
+        <Helmet title={`${pageTitle} | ${siteTitle}`} />
         <Container>
           <Columns isVCentered={true}>
             <Column isSize={3} isOffset={1}>
@@ -25,30 +27,39 @@ const ClientFormsPage = ({ data }) => {
             </Column>
             <Column>
               <div css={{ paddingLeft: '2em' }}>
-                <PageHeader title="Client Forms" />
+                <PageHeader title={pageTitle} />
                 <Content>
-                  <p css={{ marginBottom: '2em', marginRight: '1.5em' }}>
-                    For current clients, please fill out the following forms and
-                    bring them to your next appointment:
-                    <ol>
-                      <li>
-                        <a
-                          title="Initial Interview Form"
-                          href="/documents/forms/InitialInterview.pdf"
-                        >
-                          Initial Interview (PDF)
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          title="Disclaimer Form"
-                          href="/documents/forms/Disclaimer.pdf"
-                        >
-                          Disclaimer (PDF)
-                        </a>
-                      </li>
-                    </ol>
-                  </p>
+                  <div css={{ marginBottom: '2em', marginRight: '1.5em' }}>
+                    <p>
+                      I provide a free 20 minute consultation for new clients.
+                    </p>
+                    <p>
+                      Send a message on the <Link to="/contact">contact</Link>{' '}
+                      page to request an appointment.
+                    </p>
+                    <p>
+                      For current clients, please fill out the following forms
+                      and bring them to your next appointment:
+                      <ol>
+                        <li>
+                          <a
+                            title="Initial Interview Form"
+                            href="/documents/forms/InitialInterview.pdf"
+                          >
+                            Initial Interview (PDF)
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            title="Disclaimer Form"
+                            href="/documents/forms/Disclaimer.pdf"
+                          >
+                            Disclaimer (PDF)
+                          </a>
+                        </li>
+                      </ol>
+                    </p>
+                  </div>
                 </Content>
               </div>
             </Column>
@@ -56,10 +67,10 @@ const ClientFormsPage = ({ data }) => {
         </Container>
       </Section>
     </Layout>
-  )
-}
+  );
+};
 
-ClientFormsPage.propTypes = {}
+ClientFormsPage.propTypes = {};
 
 export const query = graphql`
   query ClientFormsPageQuery {
@@ -76,6 +87,6 @@ export const query = graphql`
       }
     }
   }
-`
+`;
 
-export default ClientFormsPage
+export default ClientFormsPage;
